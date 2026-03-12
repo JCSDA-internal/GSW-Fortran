d9d075b07c547656b4d0b5a411b87e68879c7a74
Francois Hebert <hebert@ucar.edu>
2026-03-12 19:19:16 +0000

    Clean up compiler flag specification

--- cmake/gsw_compiler_flags.cmake
+++ cmake/gsw_compiler_flags.cmake
@@ -1,16 +1,15 @@
-# (C) Copyright 2018 UCAR.
+# (C) Copyright 2026 UCAR
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
 
-#######################################################################################
-# Fortran
-#######################################################################################
 
-if( CMAKE_Fortran_COMPILER_ID MATCHES "GNU" )
-  include( compiler_flags_GNU_Fortran )
-elseif( CMAKE_Fortran_COMPILER_ID MATCHES "Intel" )
-  include( compiler_flags_Intel_Fortran )
-else()
-  message( STATUS "Fortran compiler with ID ${CMAKE_Fortran_COMPILER_ID} will be used with CMake default options")
-endif()
+# Set compiler flags for basic build types,
+# for compilers where this is not provided by ecbuild.
+include(build_type_compiler_flags)
+
+# Set JEDI's common compiler flags
+include(jedi_common_compiler_flags)
+
+# Set GSW-specific compiler flags
+# none
